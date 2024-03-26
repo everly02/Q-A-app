fe23c9c2064faa0be47fda2d1b345efb6e28c40d
爱敬如一 <122416188+everly02@users.noreply.github.com>
2024-03-26 13:28:26 +0800

    temp del

--- ProblemSolvingapp/app/src/main/java/com/example/problem_solvingapp/Question.java
+++
@@ -1,7 +0,0 @@
-package com.example.problem_solvingapp;
-
-public class Question {
-    public Question(String title, String content, int askerID) {
-
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Q-A-app
-this is my personal project, a q&amp;a app, allow user to ask and answer questions, and ask AI chatbot for help
